05b5f9fa63bd0673ec9502f920bc3722bafeed19
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-09-05 15:27:22 -0300

    Iniciando o react

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className="App">
+        <h1>Olá mundo</h1>
+      </main>
+    );
+  }
+}
+  
+
+export default App;
