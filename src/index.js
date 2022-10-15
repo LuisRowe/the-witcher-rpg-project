1996bc6459afa65204384803182be8073d95ea14
Luis Rowe <luisfelipe.apoloniorowe@gmail.com>
2022-10-15 11:32:06 -0300

    Caminhos da página de inicio

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import Begin from './pages/Begin';
+import CharacterCreate from './pages/CharacterCreate';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <Switch>
+      <Route exact path='/begin' component={ Begin } />
+      <Route exact path='/characterCreate' component={ CharacterCreate } />
       <Route exact path='/' component={ Home } />
     </Switch>
   </BrowserRouter>
